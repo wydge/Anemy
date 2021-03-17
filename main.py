e3f1779c4c45d4c7ac133c9983f21ea48e4678fb
Wydge <leonardo.paparella98@gmail.com>
2021-03-18 00:08:50 +0100

    caricamento Anemy in Python

---
+++ main.py
@@ -0,0 +1,29 @@
+import cv2
+cam = cv2.VideoCapture(0)
+
+cv2.namedWindow("test")
+
+img_counter = 0
+
+while True:
+    ret, frame = cam.read()
+    if not ret:
+        print("Impossibile catturare l'immagine")
+        break
+    cv2.imshow("test", frame)
+
+    k = cv2.waitKey(1)
+    if k%256 == 27:
+        # ESC per uscire
+        print("Chiusura schermata")
+        break
+    elif k == ord(' '):
+        # SPACE per scattare la foto
+        img_name = "foto_{}.jpg".format(img_counter)
+        cv2.imwrite(img_name, frame)
+        print("{} written!".format(img_name))
+        img_counter += 1
+        #break  mettendo questo ti scatta solo una foto e poi ti chiude la finestra direttamente, l'ho tolto cosi è possibile fare più scatti
+cam.release()
+
+cv2.destroyAllWindows()
